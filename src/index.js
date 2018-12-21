3f18fbbbf77c22a875037eaee19ed81a0708a6bf
Kelly Silva <kelly@kellyregina.com.br>
2018-12-20 23:29:16 -0200

    add creat html function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import style from "./main.scss";
+
+
+function createHtmlElement(size = 3){
+ const fragment = document.createDocumentFragment();
+  for(let row = 1; row <= size; row++){
+    for(let col = 1; col <= size; col++){
+      const element = document.createElement('li');
+
+      element.textContent = fragment.children.length + 1;
+      element.style.gridRow = row ;
+      element.style.gridColumn = col;
+
+      element.onclick  = tileClicked;
+
+      fragment.appendChild(element);
+    }
+  }
+  return fragment;
+}
