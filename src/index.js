9e5276b8b0fd4df890ecd070d5edae103f89ff5c
Kelly Silva <kelly@kellyregina.com.br>
2018-12-22 15:09:06 -0200

    add suffle and solve

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import style from "./main.scss";
+import style from './main.scss';
 
 const size = 3;
-const dificulty = 5;
+const dificulty = 10;
+const moves = [];
+
 const fragment = createHtmlElement(size);
 const tiles = Array.from(fragment.children);
 const blank = tiles[tiles.length - 1];
-blank.className = "white";
 
-const moves = [];
-
-for(let i = 0; i < dificulty; i++) {
-  const availableMoves = tiles.filter(canMove);
-  const randomTile = availableMoves[Math.floor(Math.random() * availableMoves.length)]
-  swap(randomTile, blank);
-  moves.push(randomTile);
-}
-
-/* UNDO MOVES
-let tile;
-while(tile = moves.pop()) {
-  console.log("caiu aqui");
-  swap(tile, blank);
-}
-*/
-
+blank.className = 'blank';
+shuffle(dificulty);
 document.getElementById('ulSliding').appendChild(fragment);
+document.getElementById('solve').onclick = solve;
+document.getElementById('shuffle').onclick = _ => shuffle(dificulty);
 
 function tileClicked(e) {
-  if(canMove(e.target)) {
-   swap(e.target, blank);
+  if (canMove(e.target)) {
+    swap(e.target, blank);
+    moves.push(e.target);
+  }
+}
+
+function shuffle(dificulty) {
+  for (let i = 0; i < dificulty; i++) {
+    const availableMoves = tiles.filter(canMove);
+    const randomTile = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+    swap(randomTile, blank);
+    moves.push(randomTile);
+  }
+}
+
+function solve() {
+  let tile;
+  while ((tile = moves.pop())) {
+    console.log('caiu aqui');
+    swap(tile, blank);
   }
 }
 
@@ -44,20 +50,21 @@ function swap(a, b) {
   [a.style.gridColumn, b.style.gridColumn] = [b.style.gridColumn, a.style.gridColumn];
 }
 
-function createHtmlElement(size = 3){
- const fragment = document.createDocumentFragment();
-  for(let row = 1; row <= size; row++){
-    for(let col = 1; col <= size; col++){
+function createHtmlElement(size) {
+  const fragment = document.createDocumentFragment();
+  for (let row = 1; row <= size; row++) {
+    for (let col = 1; col <= size; col++) {
       const element = document.createElement('li');
 
       element.textContent = fragment.children.length + 1;
-      element.style.gridRow = row ;
+      element.style.gridRow = row;
       element.style.gridColumn = col;
 
-      element.onclick  = tileClicked;
+      element.onclick = tileClicked;
 
       fragment.appendChild(element);
     }
   }
   return fragment;
 }
+
