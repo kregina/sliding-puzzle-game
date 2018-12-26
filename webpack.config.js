901d377edca21b04185e57ac0993ad90a1b584ec
Kelly Silva <kelly@kellyregina.com.br>
2018-12-26 01:18:41 -0200

    fix build prod

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,15 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
       }
     ]
   },
